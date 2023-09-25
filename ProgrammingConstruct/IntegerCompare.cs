bc5cd2526bea77fc634dc20263e2ffb9ad1ff307
Rohan <rohandute1@gmail.com>
2023-09-25 23:46:42 +0530

    [rohan]checking two integer equal or not

---
+++ ProgrammingConstruct/IntegerCompare.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct
+{
+    internal class IntegerCompare
+    {
+        public void EqualityChecker()
+        {
+            Console.WriteLine("Enter the first integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second integer: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 == num2)
+            {
+                Console.WriteLine("The two integers are equal.");
+            }
+            else
+            {
+                Console.WriteLine("The two integers are not equal.");
+            }
+        }
+    }
+}
--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -11,6 +11,32 @@ namespace ProgrammingConstruct
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Programming Construct problems");
+
+            bool continueExecution = true;
+            while (continueExecution)
+            {
+                Console.WriteLine("Please choose any one of the following program:");
+                Console.WriteLine("1.Check Two int are equal or not\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        IntegerCompare integerCompare = new IntegerCompare();
+                        integerCompare.EqualityChecker();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option, Please select a valid program.");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue?(yes or no)");
+                string userInput = Console.ReadLine().ToLower();
+                if (userInput != "yes")
+                {
+                    continueExecution = false;
+                }
+            }
+
             Console.ReadLine();
         }
     }

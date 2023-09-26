70d9aa96f7073ba02519689c9731ed079ea356e8
Rohan <rohandute1@gmail.com>
2023-09-26 08:29:02 +0530

    [rohan] Checking even or odd integer

---
+++ ProgrammingConstruct/NumberChecker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct
+{
+    internal class NumberChecker
+    {
+        public void CheckEvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is odd.");
+            }
+        }
+
+    }
+}
--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -16,13 +16,22 @@ namespace ProgrammingConstruct
             while (continueExecution)
             {
                 Console.WriteLine("Please choose any one of the following program:");
-                Console.WriteLine("1.Check Two int are equal or not\n");
+                Console.WriteLine("1.Check Two int are equal or not\n2.Check given number is even or odd.");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         IntegerCompare integerCompare = new IntegerCompare();
                         integerCompare.EqualityChecker();
+                        break;
+
+                    case 2:
+                        NumberChecker checker = new NumberChecker();
+
+                        Console.WriteLine("Enter a number: ");
+                        int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+                        checker.CheckEvenOrOdd(inputNumber);
                         break;
 
                     default:
